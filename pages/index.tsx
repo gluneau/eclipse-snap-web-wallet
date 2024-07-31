363926ee6368ffe70e911f899add651a58219113
Gregory Luneau <gregory.luneau@gmail.com>
2024-07-31 17:47:00 -0500

    table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
+import React from "react";
 import DefaultLayout from "@/layouts/default";
+import ReactClipboard from "react-clipboardjs-copy";
 import { useSolana } from "@/context/SolanaContext";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Button,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+import { Copy } from "react-feather";
 
 export default function IndexPage() {
   const { solanaAddress } = useSolana();
@@ -7,15 +24,65 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          {/* Other content */}
-        </div>
-
-        <div className="flex gap-3">
-          <p className="text-sm font-normal text-default-600">
-            Address: {solanaAddress || "Not connected"}
-          </p>
-        </div>
+        <Card className="w-5/6 max-w-[1200px]">
+          <CardHeader className="flex justify-between gap-4 items-center">
+            <div className="flex flex-col">
+              <p className="text-md font-bold">Total portfolio value</p>
+              <p className="text-2xl font-bold">$2.01</p>
+            </div>
+            <Divider orientation="vertical" />
+            <div className="flex flex-col">
+              <p className="text-md">Solana Wallet</p>
+              <div className="flex items-center">
+                <p className="text-small text-default-500">
+                  {solanaAddress || "Connect your wallet to get started"}
+                </p>
+                <button
+                  onClick={() => {
+                    console.log("click button");
+                  }}
+                  className="ml-2"
+                >
+                  <Copy size="16" />
+                </button>
+              </div>
+            </div>
+            <div className="flex gap-4 ml-auto">
+              <Button>Receive</Button>
+              <Button>Send</Button>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p className="text-lg font-bold">Assets</p>
+            <Table aria-label="Assets table">
+              <TableHeader>
+                <TableColumn>NAME</TableColumn>
+                <TableColumn>PRICE/24H CHANGE</TableColumn>
+                <TableColumn>VALUE</TableColumn>
+                <TableColumn>AMOUNT</TableColumn>
+              </TableHeader>
+              <TableBody>
+                <TableRow key="1">
+                  <TableCell>
+                    <p className="text-md font-bold">Solana</p>
+                    <p className="text-small text-default-500">SOL</p>
+                  </TableCell>
+                  <TableCell>
+                    <p className="text-md">$100.58</p>
+                    <p className="text-small text-green-600">+3.09%</p>
+                  </TableCell>
+                  <TableCell>
+                    <p className="text-md">$2.01</p>
+                  </TableCell>
+                  <TableCell>
+                    <p className="text-md">0.02</p>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardBody>
+        </Card>
       </section>
     </DefaultLayout>
   );
